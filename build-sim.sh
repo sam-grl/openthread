33ab12544de5915b529d1dbc58fe2c72654b8a19
Esko Dijk <esko.dijk@iotconsultancy.nl>
2022-07-09 21:35:36 +0200

    added firmware build files for simulation-OTNS-with-radio-model

---
+++ build-sim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./script/cmake-build simulation -DOT_PLATFORM=simulation -DOT_OTNS=ON -DOT_SIMULATION_VIRTUAL_TIME=ON -DOT_SIMULATION_VIRTUAL_TIME_UART=ON \
+ -DOT_SIMULATION_MAX_NETWORK_SIZE=999 -DOT_COMMISSIONER=ON -DOT_JOINER=ON -DOT_BORDER_ROUTER=ON -DOT_SERVICE=ON -DOT_COAP=ON \
+ -DOT_FULL_LOGS=OFF
+
+if [ $? -eq 0 ]; then
+  cp ./build/simulation/examples/apps/cli/ot-cli* ~/ot-ns
+  echo ot-cli* executables copied to ~/ot-ns
+fi
