bcbb707bfacd75716c64cee63112bb938d8fb072
Esko Dijk <esko.dijk@iotconsultancy.nl>
2024-05-14 18:31:38 +0200

    WIP - added some tracing outputs (FIXME)

--- src/core/thread/mesh_forwarder_ftd.cpp
+++ src/core/thread/mesh_forwarder_ftd.cpp
@@ -54,6 +54,7 @@ void MeshForwarder::SendMessage(OwnedPtr<Message> aMessagePtr)
     message.SetDatagramTag(0);
     message.SetTimestampToNow();
     mSendQueue.Enqueue(message);
+    LogDebg("FIXME SendMessage processing type: %u", message.GetType());
 
     switch (message.GetType())
     {
